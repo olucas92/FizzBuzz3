6b226a5dac09e3e21d7401d3cdb3b651783ee801
Oliver <o.lucas@live.com>
2015-08-27 14:42:46 +0100

    is divisible by 5 test passing

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -12,6 +12,10 @@ describe("FizzBuzz", function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it("is divisible by 5", function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
 });
 
 });
